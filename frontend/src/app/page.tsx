a786febd458c124c00d7d1d25ca05d9c77a7033e
BrenoaAlmeida <brenoalmeida95@gmail.com>
2024-12-23 15:16:53 -0400

    Criado Pagina de Login com o CSS

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,9 +1,43 @@
 import styles from './page.module.scss'
+import logoImg from  '/public/logo.svg'
+import Image from 'next/image'
+import Link from 'next/link'
 
 export default function Home(){
   return(
-    <main>
-      <h1 className={styles.teste}>TESTE TESTE</h1>
-    </main>
+    <>
+      <div className={styles.containerCenter}>
+        <Image
+          src={logoImg}
+          alt="Logo da pizzaria"
+        />
+      </div>
+
+      <section className={styles.login}>
+      <form>
+        <input
+          type="email"
+          required
+          name='email'
+          placeholder='Digite seu email...'
+          className={styles.input}
+        />        
+        <input 
+          type="password"
+          required
+          name='password'
+          placeholder='************'
+          className={styles.input}
+        />
+        <button type="submit">
+          Acessar
+        </button>
+      </form>
+
+      <Link href="/signup" className={styles.text}>
+        Não possui uma conta? Cadastre-se
+      </Link>
+      </section>
+    </>
   )
 }
---
+++ frontend/src/app/signup/page.tsx
@@ -0,0 +1,11 @@
+export default function SIgnup() {
+    return(
+        <>
+            <main>
+                <h1>
+                    Pagina Cadastro
+                </h1>
+            </main>
+        </>
+    )
+}
