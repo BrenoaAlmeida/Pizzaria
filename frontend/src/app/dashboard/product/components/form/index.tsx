637188921797c017d7d546d05dca986c5e63df81
BrenoaAlmeida <brenoalmeida95@gmail.com>
2025-01-06 19:34:39 -0400

    Criado sub paginas e componentes para a pagina  de painel

---
+++ frontend/src/app/dashboard/product/components/form/index.tsx
@@ -0,0 +1,82 @@
+"use client"
+
+import { ChangeEvent, useState } from 'react'
+import styles from './styles.module.scss'
+import { UploadCloud } from 'lucide-react'
+import Image from 'next/image'
+import { Button } from '@/app/dashboard/components/button'
+
+export default function Form() {
+    const [image, setImage] = useState<File>();
+    const [previewImage, setPreviewImage] = useState("");
+
+    function handleFile(e: ChangeEvent<HTMLInputElement>){
+        if(e.target.files && e.target.files[0]){            
+            const image = e.target.files[0];
+
+            if(image.type !== "image/jpeg" && image.type !== "image/png"){
+                console.log("Formato Proibido!!!")
+                return;
+            }
+
+            setImage(image);
+            //Criar URL de Preview para mostrar a imagem
+            setPreviewImage(URL.createObjectURL(image))
+        }
+    }
+    return(
+        <main className={styles.container}>
+            <h1>Novo Produto</h1>
+            <form className={styles.form}>
+                <label className={styles.labelImage}>
+                <span>
+                    <UploadCloud size={30} color="#FFF" />
+                </span>
+                <input 
+                type='file'
+                accept='image/png, imagem/jpeg'
+                required
+                onChange={handleFile} />
+                {previewImage && (
+                    <Image
+                    alt="Imagem de preview"
+                    src={previewImage}
+                    className={styles.preview}
+                    fill={true}
+                    quality={100}
+                    priority={true} />
+                )}
+                </label>
+                <select name="category">
+                    <option key={1} value={1}>Pizzas</option>
+                    <option key={1} value={1}>Massas</option>
+                </select>
+
+                <input
+                type='text'
+                name='name'
+                placeholder='Digite o nome do produto'
+                required
+                className={styles.input} 
+                />
+
+                <input
+                type='text'
+                name='price'
+                placeholder='Digite o preço do produto'
+                required
+                className={styles.input} 
+                />
+
+                <textarea                
+                name='description'
+                placeholder='Digite a descrição do produto'
+                required
+                className={styles.input} 
+                />
+
+                <Button name="Cadastrar Produto"/>
+            </form>
+        </main>
+    )
+}
---
+++ frontend/src/app/dashboard/product/page.tsx
@@ -0,0 +1,11 @@
+import Form from "./components/form";
+
+export default function Product(){
+    return(
+        <main>
+            <h1>Pagina Product </h1>
+
+            <Form/>
+        </main>     
+    )
+}
