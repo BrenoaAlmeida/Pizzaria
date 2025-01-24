197e197f9b4c31d3962faf9ffa88ae947d9480c6
BrenoaAlmeida <brenoalmeida95@gmail.com>
2025-01-24 15:50:43 -0400

    atualizacao de componentes

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -6,14 +6,11 @@ import path from 'path'
 import { router } from './routes';
 
 const app = express();
-
 app.use(express.json());
-
+app.use(cors());
 app.use(router);
-
 app.use('/files', express.static(path.resolve(__dirname, '..', 'tmp'))); //Cria uma rota estatica e passa /files/
 
-app.use(cors);
 
 //Usar middleWare para tratar erros
 app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
--- frontend/src/app/dashboard/components/orders/index.tsx
+++ frontend/src/app/dashboard/components/orders/index.tsx
@@ -1,32 +1,61 @@
+"use client"
+
+import { use } from 'react'
 import styles from './styles.module.scss'
 import { RefreshCcw } from 'lucide-react'
 import { OrdersProp } from '@/lib/order.type'
+import { Modalorder } from '../modal';
+import { OrderContext } from '@/providers/order'
+import { useRouter } from 'next/navigation'
+import { toast } from 'sonner'
 
 interface Props{
     orders: OrdersProp[];
 } 
 
 export default function Orders({orders}: Props){
+    const { isOpen, onRequestOpen } = use(OrderContext)
+    const router = useRouter();
+
+    async function handleDetailOrder(order_id:string){
+        await onRequestOpen(order_id);
+    }
+
+    function handleRefresh() {
+        router.refresh();
+        toast.success("Pedidos atualizados com sucesso!")
+    }
+
     return(
+        <>
         <main className={styles.container} >
             <section className={styles.containerHeader}>
                 <h1>Ultimos pedidos</h1>
-                <button>
+                <button onClick={handleRefresh}>
                     <RefreshCcw size={24} color="#3fffa3" />
                 </button>
             </section>
 
             <section className={styles.listOrders}>
+                {orders.length === 0 && (
+                    <span className={styles.emptyItem}>
+                        Nenhum pedido aberto no momento!
+                    </span>
+                )}
+
                 {orders.map(order => (
                     <button
                         key={order.id}
                         className={styles.orderItem}
+                        onClick={() => handleDetailOrder(order.id)}
                     >
                         <div className={styles.tag} />                        
                         <span>Mesa {order.table}</span>                                        
                     </button>
                 ))}
             </section>
-        </main>                
+        </main>                        
+        {isOpen && <Modalorder/>}        
+        </>// Fragment é uma  tag sem estilização para não atrapalhar         
     )
 }
